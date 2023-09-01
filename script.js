21cc9f9a9e54465edde877344cdfebbfa4cbdb9d
victor-avancini <victor.avancini@hotmail.com>
2023-09-01 20:24:19 -0300

    Atualização rubrica

--- script.js
+++ script.js
@@ -1,25 +1,10 @@
 function playRockPaperScissor(player1,player2) {
-    if (player1 == 'Pedra'){
-        if (player2 == 'Papel') {
-            return 'Jogador 2 venceu!'
-        } else if (player2 == 'Tesoura') {
-            return 'Jogador 1 venceu!'
-        }
+    if (player1 == player2) {
         return 'Empate!'
-    } else if (player1 == 'Papel') {
-        if (player2 == 'Pedra') {
-            return 'Jogador 1 venceu!'
-        } else if (player2 == 'Tesoura') {
-            return 'Jogador 2 venceu!'
-        }
-        return 'Empate!'
-    }
-    if (player2 == 'Pedra') {
+    } else if (player1 == 'Pedra' && player2 == 'Papel' || player1 == 'Papel' && player2 == 'Tesoura' || player1 == 'Tesoura' && player2 == 'Pedra'){
         return 'Jogador 2 venceu!'
-    } else if (player2 == 'Papel') {
-        return 'Jogador 1 venceu!'
     }
-    return 'Empate!'
+    return 'Jogador 1 venceu!'
 }
 
 const resultado = playRockPaperScissor('Pedra','Papel');
