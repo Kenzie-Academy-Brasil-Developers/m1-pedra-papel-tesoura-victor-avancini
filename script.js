3aa58ec83147fcd84d9a015c55b0fcc690f8579b
victor-avancini <victor.avancini@hotmail.com>
2023-09-04 15:31:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ function playRockPaperScissor(player1,player2) {
         return 'Empate!'
     } else if (player1 == 'Pedra' && player2 == 'Papel' || player1 == 'Papel' && player2 == 'Tesoura' || player1 == 'Tesoura' && player2 == 'Pedra'){
         return 'Jogador 2 venceu!'
+    } else {
+        return 'Jogador 1 venceu!'
     }
-    return 'Jogador 1 venceu!'
 }
 
 const resultado = playRockPaperScissor('Pedra','Papel');
